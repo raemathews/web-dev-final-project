66a23fb3af76e41e0542349873df6562f3963334
Andra <andra.ioana2002@gmail.com>
2023-04-18 16:23:54 -0400

    fixed merge conflicts for app.js

--- src/App.js
+++ src/App.js
@@ -11,27 +11,36 @@ import ProfileReads from "./book-review/profile/ProfileReads";
 import ProfileWillReads from "./book-review/profile/ProfileWillReads";
 import SignUp from "./book-review/signUp/SignUp";
 
+import {configureStore} from "@reduxjs/toolkit";
+import libraryReducer from "./reducers/library-reducer";
+import usersReducer from './reducers/users-reducer';
+import {Provider} from "react-redux";
+
+const store = configureStore(
+    {reducer: {library: libraryReducer,
+        users: usersReducer}});
+
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route index element={<Home />} />
-
-        <Route path="login" element={<Login />}/>
-          <Route path="profile" element={<Profile />} />
-          <Route path="book-details/*" element={<BookDetails />} />
-          <Route path="search/*" element={<Search />} />
-        <Route path="/login" element={<Login />}/>
-          <Route path="/profile" element={<Profile active="start"/>} />
-          <Route path="/profile/followers" element={<ProfileFollowers active="followers"/>} />
-          <Route path="/profile/following" element={<ProfileFollowing active="following"/>} />
-          <Route path="/profile/reads" element={<ProfileReads active="reads"/>} />
-          <Route path="/profile/willReads" element={<ProfileWillReads active="willReads"/>} />
-          <Route path="/book-details" element={<BookDetails />} />
-          <Route path="/search" element={<Search />} />
-        <Route path="/signUp" element={<SignUp />}/>
-      </Routes>
-    </Router>
+      <Provider store={store}>
+        <Router>
+          <Routes>
+            <Route index element={<Home/>}/>
+            <Route path="login" element={<Login/>}/>
+            <Route path="profile" element={<Profile/>}/>
+            <Route path="book-details/*" element={<BookDetails/>}/>
+            <Route path="search/:query" element={<Search/>}/>
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/profile" element={<Profile active="start"/>}/>
+            <Route path="/profile/followers" element={<ProfileFollowers active="followers"/>}/>
+            <Route path="/profile/following" element={<ProfileFollowing active="following"/>}/>
+            <Route path="/profile/reads" element={<ProfileReads active="reads"/>}/>
+            <Route path="/profile/willReads" element={<ProfileWillReads active="willReads"/>}/>
+            <Route path="/book-details" element={<BookDetails/>}/>
+            <Route path="/search" element={<Search/>}/>
+          </Routes>
+        </Router>
+      </Provider>
   );
 }
 
