18a89166a856d187728c646fe99d869baee72720
Andra <andra.ioana2002@gmail.com>
2023-04-19 13:34:24 -0400

    fixed refactor

--- src/book-review/login/Login.js
+++ src/book-review/login/Login.js
@@ -1,25 +1,48 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useNavigate } from "react-router";
+import { useDispatch } from "react-redux";
+import { loginThunk } from "../services/auth-thunks";
 import Navigation from "../navigation/Navigation";
-import SignUp from "../signUp/SignUp";
 import {Link} from "react-router-dom";
-
-const Login = () => {
+function Login() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const handleLogin = async () => {
+        try {
+            await dispatch(loginThunk({ username, password }));
+            navigate("/profile");
+        } catch (e) {
+            alert(e);
+        }
+    };
     return (
         <>
             <Navigation />
-            <h1>Login</h1>
-            <h3>Please enter your username and password</h3>
-            <form id="text-fields">
-                <label htmlFor="text-fields-username">Username: </label>
-                <input id="text-fields-username"/><br/>
-                <label htmlFor="text-fields-password">Password: </label>
-                <input type="password" id="text-fields-password"
-                /><br/>
-            </form>
+            <div>
+                <h1>Login Screen</h1>
+                <div>
+                    <label>Username</label>
+                    <input className="form-control"
+                           type="text" value={username}
+                           onChange={(event) => setUsername(event.target.value)}
+                    />
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input className="form-control"
+                           type="password" value={password}
+                           onChange={(event) => setPassword(event.target.value)}
+                    />
+                </div>
+                <button onClick={handleLogin}>
+                    Login
+                </button>
+            </div>
             <br/>
             <br/>
             <br/>
-
             <h3>Not a User Yet? Click here to sign up!</h3>
             <button className="btn btn-warning">
                 <Link to="/signUp">
@@ -27,9 +50,10 @@ const Login = () => {
                 </Link>
 
             </button>
-
         </>
-    );
-}
 
-export default Login
+    );
+
+
+}
+export default Login;
