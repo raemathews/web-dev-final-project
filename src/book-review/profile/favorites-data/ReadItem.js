7feb7a7fb765d5cf4b73e89c7e79f7b442fd1a51
Andra <andra.ioana2002@gmail.com>
2023-04-23 00:51:41 -0400

    all tabs for profiles are linkedgit add . !

--- src/book-review/profile/favorites-data/ReadItem.js
+++ src/book-review/profile/favorites-data/ReadItem.js
@@ -6,13 +6,19 @@ const ReadItem = (
         <li className="list-group-item">
             <div className="row">
                 <div className="col-10">
-                    <div className="fw-bolder">{book.book_title}
-                        - {book.publish_date}
+                    <div className="fw-bolder">{book.title}
+                        - {book.rating_average}
                     </div>
-                    <div>{book.summary}</div>
+                    <div>By {book.author_name}</div>
                 </div>
                 <div className="col-2">
-                    <img width={70} className="float-end rounded-3" src={`/images/${book.image}`}/>
+                    {book.cover_i ?
+                        <img className="card-img-top"
+                             src={`https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg`}
+                             alt="book cover"/>
+                        : <img className="card-img-top not-found"
+                               src={"/images/no_cover.png"}
+                               alt="book cover"/>               }
                 </div>
             </div>
         </li>
